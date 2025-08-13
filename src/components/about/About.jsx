91761546dc66f11860aad08bad17693592b30bd1
Yagya <143512277+CodeCraftman2@users.noreply.github.com>
2025-08-13 02:56:12 -0700

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./about.css";
 import AboutImg from "../../assets/aboutBW.webp";
-import CV from "../../assets/Example-Cv.pdf";
+import CV from "../../assets/Yagya_Saini_Resume.pdf";
 import Info from "./Info";
 
 const About = () => {
